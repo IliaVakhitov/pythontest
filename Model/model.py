e3badb58010b5c974137f4ebb0149481dadb39a2
iv7180 <iv7180@gmail.com>
2019-10-18 15:29:43 +1000

    Generate json data from dictionary. Write dictionary to json.

--- Model/model.py
+++ Model/model.py
@@ -1,9 +1,10 @@
+import json
 
 class Model:
     pass
 
 
-class DictEntry():
+class DictEntry:
     word = ""
     translation = ""
     learnIndex = 0
@@ -12,29 +13,57 @@ class DictEntry():
         self.word = word
         self.translation = translation
 
-    def setLearnIndex(self, value):
+    def set_learn_index(self, value):
         self.learnIndex = value
 
-    def setWord(self, value):
+    def increase_learn_index(self):
+        self.learnIndex += 1
+
+    def decrease_learn_index(self):
+        self.learnIndex -= (1 if self.learnIndex >= 1 else 0)
+
+    def set_word(self, value):
         self.word = value
 
-    def setTranslation(self, value):
+    def set_translation(self, value):
         self.translation = value
 
-    def printEntry(self):
+    def print_entry(self):
         return "{} - {} : {}".format(self.word, self.translation, self.learnIndex)
 
 
 class Dictionary:
     words = []
+    name = ""
+    json_data = {}
 
-    def addNewEntry(self, word, translation):
+    def __init__(self, dict_name):
+        self.name = dict_name
+
+    def write_to_json(self, filename):
+        self.generate_json_data()
+        with open(filename, 'w') as outfile:
+            json.dump(self.name, outfile)
+            json.dump(self.json_data, outfile)
+
+    def generate_json_data(self):
+        json_data = {'name': self.name, 'words': []}
+        for word in self.words:
+            json_data['words'].append({
+                'word': word.word,
+                'translation': word.translation,
+                'learnIndex': word.learnIndex,
+            })
+
+        self.json_data = json_data
+
+    def add_new_entry(self, word, translation):
         self.words.append(DictEntry(word, translation))
 
-    def printWords(self):
+    def print_words(self):
         res = ""
         for word in self.words:
-            res = res + word.printEntry()
+            res = res + word.print_entry()
             res = res + "\n"
 
         return res
--- Start.py
+++ Start.py
@@ -1,12 +1,12 @@
 from View import view
 from Model import model
 
+my_dict = model.Dictionary("English")
+my_dict.add_new_entry("one", "один")
+my_dict.add_new_entry("two", "два")
+my_dict.add_new_entry("three", "три")
+my_dict.add_new_entry("four", "четыре")
 
-
-dict = model.Dictionary()
-dict.addNewEntry("one", "один")
-dict.addNewEntry("two", "два")
-dict.addNewEntry("three", "три")
-dict.addNewEntry("four", "четыре")
-view.printStr(dict.printWords())
-
+view.printStr(my_dict.name)
+my_dict.write_to_json(my_dict.name + ".json")
+view.printStr(my_dict.json_data)
